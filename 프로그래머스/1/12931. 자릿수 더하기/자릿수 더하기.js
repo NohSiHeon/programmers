822ef2ff912bf9b6a1a2203110abd30315f73a19
NohSiHeon <91312161+NohSiHeon@users.noreply.github.com>
2024-04-26 11:34:17 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -7,12 +7,6 @@ function solution(n)
         answer += parseInt(newN[i]);
     }
  
-    
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    
-//     let n100 = parseInt(n / 100);
-//     let n10 = parseInt(n % 100 / 10);
-//     let n1 = parseInt(n % 10);
-//     answer = n100 + n10 + n1;
+
     return answer;
 }
