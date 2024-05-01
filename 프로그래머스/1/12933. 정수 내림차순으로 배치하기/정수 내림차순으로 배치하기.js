f9ecc3282c707ddeb23f99f9e28187c4221e51fc
NohSiHeon <91312161+NohSiHeon@users.noreply.github.com>
2024-05-01 10:00:05 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = 0;
+    let arr = String(n).split('').sort().reverse().join('');
+    answer = Number(arr);
+    
+    
+
+    
+    
+    return answer;
+}
