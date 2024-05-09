a5e52501b2fc600fe0fe11ca1d49676e966c1966
NohSiHeon <91312161+NohSiHeon@users.noreply.github.com>
2024-05-09 09:41:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,10 +1,16 @@
+// function solution(phone_number) {
+//     let answer = '';
+//     for(let i = 0; i < phone_number.length - 4; i++){
+//         answer += phone_number[i].replace(phone_number[i], "*");
+//     }
+//     answer = answer + phone_number.substring(phone_number.length - 4);
+
+//     return answer;
+// }
+
 function solution(phone_number) {
     let answer = '';
-    phone_number
-    for(let i = 0; i < phone_number.length - 4; i++){
-        answer += phone_number[i].replace(phone_number[i], "*");
-    }
-    answer = answer + phone_number.substring(phone_number.length - 4);
+    answer = "*".repeat(phone_number.length - 4) + phone_number.substring(phone_number.length - 4);
 
     return answer;
 }
