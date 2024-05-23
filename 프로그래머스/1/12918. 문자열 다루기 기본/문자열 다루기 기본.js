a5cad18cd23be55dea106094f8b29c5bb43b5105
NohSiHeon <91312161+NohSiHeon@users.noreply.github.com>
2024-05-23 14:17:57 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -2,10 +2,13 @@ function solution(s) {
     let answer = false;
     const numS = parseInt(s);
     
-    // 문자열 s의 길이가 4 혹은 6 이고, 숫자로만 구성돼있는지 확인
+    // 문자열 s의 길이가 4 혹은 6 이고,
+    // 정수로 변환한 numS 와 s의 값이 같은지 비교
+    // 같으면 문자가 없다는 뜻, 같지 않으면 숫자
     if((s.length == 4 || s.length == 6) && numS == s){
         // if() 안이 true이면 answer = true
         answer = true;
+        
         // 아니면 false
     } else {
         answer = false;
