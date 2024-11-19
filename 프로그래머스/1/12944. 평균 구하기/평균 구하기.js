7a7b92e9e52e2e4a19cff18991c724b61d32505d
NohSiHeon <shtlgjs016@naver.com>
2024-11-19 21:55:43 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,7 +1,6 @@
 function solution(arr) {
-    var answer = 0;
-    for(let i = 0; i<arr.length; i++){
-        answer += arr[i];
-    }
-    return answer/arr.length;
+    let answer = 0;
+    
+    answer = arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
+    return answer;
 }
