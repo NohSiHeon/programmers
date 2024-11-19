bebcf61ebffbff2e57df54ecb33c14854fa22944
NohSiHeon <shtlgjs016@naver.com>
2024-11-19 15:15:03 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,12 +1,11 @@
-function solution(n)
-{
-    var answer = 0;
-    let newN = String(n);
+function solution(n) {
+    let answer = 0;
+    // 숫자를 문자열로 변환
+    n = n + "";
     
-    for(let i = 0; i < newN.length; i ++){
-        answer += parseInt(newN[i]);
+    // 문자열의 각 문자를 숫자로 변환하여 answer에 누적
+    for(let i = 0; i < n.length; i++){
+        answer += +n[i];
     }
- 
-
     return answer;
 }
