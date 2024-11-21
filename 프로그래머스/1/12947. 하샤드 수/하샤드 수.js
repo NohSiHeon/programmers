c2b4992249622e1535b72ff4ddf781a11892ad8c
NohSiHeon <shtlgjs016@naver.com>
2024-11-21 18:01:12 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,10 +1,11 @@
 function solution(x) {
     let answer = true;
-    let arr = String(x).split('').map(Number);
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++){
-        sum += arr[i];
-    }
-    answer = x % sum == 0 ? true : false;
+    // 1. 숫자 x를 문자열로 변환
+    // 2. 문자열을 구성하는 문자들을 요소로 갖는 배열로 변환
+    // 3. 각 요소들을 숫자로 변환하여 총 합을 할당
+    const totalSumSplitX = String(x).split("").reduce((acc, cur) => acc + +cur, 0);
+    // x를 자릿수의 합으로 나누었을 때 나누어 떨어지면 true, 아니면 false 할당
+    answer = x % totalSumSplitX == 0 ? true : false;
+    
     return answer;
 }
