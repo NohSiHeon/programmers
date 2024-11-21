ad25df009a156c19e8eb4ed4a9304870fe101928
NohSiHeon <shtlgjs016@naver.com>
2024-11-21 17:38:08 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,7 +1,18 @@
 function solution(n) {
-    let answer = 0
-    let x = Math.sqrt(n);
-    answer = x === parseInt(x) ? (x + 1)**2 : -1;
+    let answer = 0;
     
+    // 제곱근 구하기
+    const sqrtNum = Math.sqrt(n);
+    
+    // 구한 제곱근과 제곱근에서 소수점 버려서 같은지 비교
+    // 같을 경우 정수
+    if(sqrtNum == Math.floor(sqrtNum)){
+        // 제곱근 + 1 한 값의 제곱한 값을 answer에 할당
+        answer = (sqrtNum + 1) ** 2;
+    // 다를 경우 정수가 아님
+    } else {
+        // -1을 answer에 할당
+        answer = -1;
+    }
     return answer;
 }
