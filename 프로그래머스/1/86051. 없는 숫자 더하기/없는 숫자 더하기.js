a66809e4a2c856ecf9e72ec2cacea4e7a3bd7e87
NohSiHeon <shtlgjs016@naver.com>
2024-11-23 05:32:20 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,9 @@
 function solution(numbers) {
     let answer = 0;
-    for(let i = 0; i < 10; i++){
-        if(!numbers.includes(i)){
-            answer += i;
-        }
+
+    for(let i = 1; i <= 9; i++){
+        // 요소에 i가 있으면 0, 없으면 i값 누적
+        answer += numbers.includes(i) ? 0 : i; 
     }
     return answer;
 }
