49ec249d54ba1ffc3292b22bb19d7e525484873a
NohSiHeon <shtlgjs016@naver.com>
2024-11-23 05:05:52 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,17 +1,21 @@
 function solution(num) {
     let answer = 0;
     
-    while(num != 1){
-        if( num % 2 == 0){
+    while(num !== 1){
+        // 해당 반복을 500번 했을 경우
+        if(answer == 500){
+            answer = -1;
+            break;
+        }
+        // 짝수인 경우
+        if(num % 2 == 0){
             num = num / 2;
+        // 홀수인 경우
         } else {
             num = num * 3 + 1;
         }
         answer++;
     }
-    if(answer <= 500){
-        return answer;
-    } else {
-        return answer = -1;
-    }
+    
+    return answer;
 }
