8b5ee1a8f120943d3a2ea9dc3e8d57a13fa04466
NohSiHeon <shtlgjs016@naver.com>
2024-11-23 07:09:06 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.53 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,15 +1,16 @@
 function solution(arr) {
     let answer = [];
-    
-    if(arr.length < 2){
+    // 가장 작은 값 제거
+    // arr 배열에 가장 작은값의 인덱스부터 1개만 제거
+    arr.splice(arr.indexOf(Math.min(...arr)), 1);
+
+    // 배열이 빈 배열일 경우
+    if(arr.length == 0){
+        // answer에 -1 추가
         answer.push(-1);
-    }
-    else {
-        arr.splice(arr.indexOf(Math.min(...arr)), 1);
+    } else {
+        // answer에 arr 대입
         answer = arr;
     }
-    
-    
-    
     return answer;
 }
