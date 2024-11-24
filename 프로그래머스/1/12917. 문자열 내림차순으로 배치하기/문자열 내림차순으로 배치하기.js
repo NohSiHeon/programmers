5269c6dc753c1a1bfaf7233e7847803ebbbe4ed0
NohSiHeon <shtlgjs016@naver.com>
2024-11-24 10:43:57 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.16 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,6 +1,10 @@
 function solution(s) {
     let answer = '';
-    answer = s.split('').sort().reverse().join('');
+    // split으로 각 문자를 요소 갖는 배열로 만듦
+    // sort로 문자열 정렬: 기본 정렬 순서는 문자열의 유니코드 코드
+    // reverse로 요소의 순서 뒤집기
+    // join으로 각 요소를 합쳐서 문자열로 변환
+    answer = s.split("").sort().reverse().join("");
     
     return answer;
 }
