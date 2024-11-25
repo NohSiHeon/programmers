6b3647715ca3f156f23ac87a1ebc98b83082f9f0
NohSiHeon <shtlgjs016@naver.com>
2024-11-25 16:32:44 +0900

    [level 1] Title: 행렬의 덧셈, Time: 9.34 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,13 +1,15 @@
 function solution(arr1, arr2) {
     let answer = [];
-
-    for(let i = 0; i < arr1.length; i++){
-        let sumArr = [];
+    
+    
+    for(let i = 0 ; i < arr1.length; i++){
+        // 배열의 길이만큼 2차원 배열 만들기
+        answer.push([]);
         for(let j = 0; j < arr1[i].length; j++){
-                    
-            sumArr.push(arr1[i][j] + arr2[i][j]);
+            // 각 자리에 맞는 값 끼리 더해서 answer에도 각 자리에 맡게 할당
+            answer[i][j] = arr1[i][j] + arr2[i][j];
         }
-        answer.push(sumArr);
     }
+    
     return answer;
 }
