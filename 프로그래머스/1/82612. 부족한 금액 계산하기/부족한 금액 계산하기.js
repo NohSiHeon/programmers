1fbc6b54c7c9615c5a1ce998531d134e24c91a64
NohSiHeon <shtlgjs016@naver.com>
2024-11-25 13:18:57 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,10 +1,14 @@
 function solution(price, money, count) {
     let answer = -1;
+    // 총 비용
     let total = 0;
-    for(let i = 1; i <= count; i++){
-        total += price * i
-        answer = total < money ? 0 : total - money;
-    }
     
+    for(let i = 1; i <= count; i++){
+        // 비용 누적
+        total += price * i;
+    }
+    // 가지고 있던 금액이 비용보다 많을 경우 0, 적을 경우 부족한 금액 할당
+    answer = money > total ? 0 : total - money;
+
     return answer;
 }
