f7ee756eee3e3e3e81237460d0dc10a0e4d8133e
NohSiHeon <shtlgjs016@naver.com>
2024-11-26 15:29:52 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,18 +1,22 @@
 function solution(n, m) {
     let answer = [];
-    const numbers = [n, m];
-    numbers.sort((a, b) => a - b);
-    let [min, max] = numbers;
+    // 최대공약수
+    let gcd= 1;
+    // 최소공배수
+    let lcm = 1;
     
-    let cd = [];
-    for(let i = 1; i <= min; i++){
-        if(min % i ===0 && max % i === 0){
-            cd.push(i);
+    // 최대공약수 구하기
+    for(let i = 1; i <= n; i++){
+        // 작은 숫자부터 반복을 시작해서 가장 마지막에 조건에 만족하는 i가 최대공약수
+        if(n % i == 0 && m % i == 0){
+            gcd = i;
         }
-    }    
-    answer.push(Math.max(...cd));
-    answer.push((max * min)/Math.max(...cd));
-
+    }
     
+    // 최소공배수 구하기
+    // 두 수를 곱하고 최대공약수로 나누면 최소공배수
+    lcm = n * m / gcd;
+
+    answer.push(gcd, lcm)
     return answer;
 }
