62155f85a4f0dcd2f0dd01eb240dda56d1d90771
NohSiHeon <shtlgjs016@naver.com>
2024-11-26 22:05:52 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,20 +1,26 @@
 function solution(s) {
-    let answer = '';
-    
-    // 공백 기준으로 나뉘어서 단어로 나뉘어진 배열로 만들기
-    let newS = s.split(" ");
-    
-    // 단어로 나뉘어진 배열의 길이 만큼 반복
-    for(let i = 0; i < newS.length; i++){
-        // 배열의 각 인덱스 값의 길이만큼 반복
-        for(let j = 0; j < newS[i].length; j++){
-            // 단어의 짝/홀수에 맞게 대소문자 구별해서 값 넣기
-            answer += j % 2 == 0 ? newS[i][j].toUpperCase() : newS[i][j].toLowerCase();
+    let answer = [];
+    // 공백을 기준으로 문자열을 단어 단위로 나눔
+    s = s.split(" ");
+
+    s = s.map(x => {
+        // 단어 x를 구성하는 각 문자를 배열로 변환
+        x = x.split("");
+        for(let i = 0; i < x.length; i++){
+            // 인덱스가 홀수인 경우 소문자로 변환
+            if(i % 2 == 1){
+                x[i] = x[i].toLowerCase();
+            // 인덱스가 짝수인 경우 대문자로 변환
+            } else {
+                x[i] = x[i].toUpperCase();
+            }
         }
-        // 단어마다 구분을 위해 공백 추가
-        // 맨 마지막에는 공백이 필요없으니 마지막을 알기 위한 조건
-        // 배열의 마지막 인덱스에 도달하면 ' '(공백)이 아닌 ''을 넣어서 공백 없게 하기
-        answer += i < newS.length - 1 ? ' ': '';
-    }    
+        // 배열을 다시 문자열로 변환
+        x = x.join("");
+        // 변환된 문자열을 answer 배열에 추가
+        answer.push(x);
+    });
+    // 단어들을 공백으로 구분하여 하나의 문자열로 합침
+    answer = answer.join(" ");
     return answer;
 }
