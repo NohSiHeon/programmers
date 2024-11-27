fcd7c1ff01e1c8dafb23c3344ce765c504550b9a
NohSiHeon <shtlgjs016@naver.com>
2024-11-28 00:19:21 +0900

    [level 1] Title: 삼총사, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -2,18 +2,13 @@ function solution(number) {
     let answer = 0;
     
     for(let i = 0; i < number.length; i++){
-        for(let j = i + 1; j <number.length; j++){
+        for(let j = i + 1; j < number.length; j++){
             for(let k = j + 1; k < number.length; k++){
-                let sum = 0;
-                sum = number[i] + number[j] + number[k];
-                answer += sum === 0 ? 1 : 0;
+                // 숫자 3개의 합이 0이면 경우의 수 1증가
+                if(number[i] + number[j] + number[k] == 0) answer++;
             }
         }
     }
-    
-    // if(){
-    //     answer += 1;
-    // }
     
     return answer;
 }
