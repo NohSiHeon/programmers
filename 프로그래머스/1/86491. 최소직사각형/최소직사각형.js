d869e4c497e5e36a064ea06e56a9f7f3d6d9f776
NohSiHeon <shtlgjs016@naver.com>
2024-11-28 14:59:30 +0900

    [level 1] Title: 최소직사각형, Time: 4.36 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,24 +1,17 @@
 function solution(sizes) {
     let answer = 0;
-    let bigger = [];
-    let smaller = [];
+    // 가로 세로 통합 더 긴건 longer, 짧은건 shorter에 할당
+    let longer = [];
+    let shorter= [];
     
-    
-    // 배열 길이만큼 반복
+    // 각 요소에 더 긴 것과 짧은 것을 구분지어서 저장
+    // 이렇게 하면 
     for(let i = 0; i < sizes.length; i++){
-        // [i][0]요소가 [i][1] 요소보다 클 경우
-        // bigger에는 [i][0], smaller에는 [i][1] push
-        if(sizes[i][0] > sizes[i][1]){
-            bigger.push(sizes[i][0]);
-            smaller.push(sizes[i][1]);
-        // [i][0]요소가 [i][1] 요소보다 작을 경우
-        // bigger에는 [i][0], smaller에는 [i][1] push
-        } else {
-            bigger.push(sizes[i][1]);
-            smaller.push(sizes[i][0]);
-        }
+        longer.push(Math.max(...sizes[i]));
+        shorter.push(Math.min(...sizes[i]));
     }
-    // 큰 값 모임 중 가장 큰 값과 작은 값 모임 중 가장 큰 값 곱하기
-    answer = Math.max(...bigger) * Math.max(...smaller);
+    
+    answer = Math.max(...longer) * Math.max(...shorter);
+    
     return answer;
 }
