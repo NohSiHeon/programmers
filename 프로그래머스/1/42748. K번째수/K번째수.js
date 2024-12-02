6d27600f1a95bda47319f8be735d271a9bbc6898
NohSiHeon <shtlgjs016@naver.com>
2024-12-03 00:21:17 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,21 +1,20 @@
 function solution(array, commands) {
     let answer = [];
+    let temp = [];
     
-    // commands 배열의 길이만큼 반복
-    for(let i = 0; i < commands.length; i++){
+    commands.map((i) => {
         
-        let tempArr = [];
-        // i부터 j까지 배열 자르기
-        tempArr = array.slice(commands[i][0] - 1, commands[i][1]);
+        // array에서 i[0]-1부터 i[1]까지 자름 
+        temp = array.slice(i[0] - 1, i[1]);
         
-        // 배열 정렬하기
-        tempArr.sort((a, b) => a - b);
-
-        // 인덱스 값 구하기
-        let k = commands[i][2];
-
-        // 구한 값을 결과 배열에 넣기
-        answer.push(tempArr[k - 1] );
-    }    
+        // 잘라낸 배열을 오름차순 정렬
+        temp.sort((a, b) => a - b);
+        
+        // 잘라내고 정렬한 배열에서 i[2]-1번째 값을 결과 배열에 추가
+        answer.push(temp[i[2] - 1]);
+        
+        // temp 초기화
+        temp = [];
+    })
     return answer;
 }
