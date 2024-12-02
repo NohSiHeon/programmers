4762a7f2a5cbec1f3674afb79fb57c73d34951e5
NohSiHeon <shtlgjs016@naver.com>
2024-12-02 22:50:12 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.12 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,18 +1,12 @@
 function solution(strings, n) {
-
-    // 1. 특정 인덱스 가져오기
-    for(let i = 0; i < strings.length; i++){
-        strings[i] = strings[i][n] + strings[i];
-    }
-
-    // 2. 인덱스 값 사전순 정렬
-    strings.sort();
-
-    // 3. 요소 값 원 상태로 되돌리기
-    // replace(): 첫 번째 인자를 두 번째 인자로 변경
-    for(let j = 0; j < strings.length; j++){
-        strings[j] = strings[j].replace(strings[j][0], "")
-    }
-
-    return strings;
+    let answer = [];
+    strings.sort((a, b) => {
+        if(a[n] == b[n]){
+            return a > b ? 1 : a < b ? -1 : 0;
+        } else {
+            return a[n] > b[n] ? 1 : -1;
+        }
+    });
+    answer = strings;
+    return answer;
 }
