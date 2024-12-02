60b250e2a7d75837f839e8d0433f71691b03ce27
NohSiHeon <shtlgjs016@naver.com>
2024-12-02 21:20:18 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.64 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,29 +1,17 @@
 function solution(food) {
-    let answer = '';
-    // 0오는 구간을 마지막이라고 생각하고 0 전에 붙은 값을 reverse 해서 붙이면 끝 아닌가???
+    let answer = [];
+    // 물은 항상 처음에 오기때문에 가장 먼저 오는 요소 제거
+    food.shift();
     
-    // 총 길이 구하기
-    // 중간 인덱스에는 0(0번 인덱스 값) 이 들어가게 하기
-    for(let i = 1; i < food.length; i++){
-        // 음식 배치를 양 옆에 해야 하므로 2로 나누기
-        let foodCount = parseInt(food[i]/2);
-        // 양 옆에 배치할 한 음식의 각 개수가 1개가 안되면 다음 과정을 생략함
-        if(foodCount < 1){
-            continue;
-        }
-        // 왼쪽부터 음식 배치하기
-        // 각 음식의 개수에 맞게 나열하여 배치
-        for(let j = 0; j < foodCount; j++){
-            answer += i.toString();
+    // 같은 개수를 먹어야 하기 때문에 2로 나눠서 몫만큼만 해당 음식의 번호값 추가
+    for(let i = 0; i < food.length; i++){
+        for(let j = 0; j < Math.floor(food[i] / 2); j++){
+            answer.push(i + 1);
         }
     }
-    // 반대편 음식
-    let half = answer.split('');
-    // 서로 양 끝에서부터 먹기 시작해야 하므로 왼쪽과 순서를 반대로 하기
-    half = half.reverse().join('');
-    // 중간에 물인 0 배치
-    answer += "0" + half;
+    
+    // 기존 음식 순서 + 물 + 음식 순서 뒤집기
+    answer = answer.join("") + "0" + answer.reverse().join("");
+
     return answer;
-    
-    
 }
