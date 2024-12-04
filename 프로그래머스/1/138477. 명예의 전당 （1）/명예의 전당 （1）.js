4cd1f8bfe6b818e92e278d9c17815c412293f872
NohSiHeon <shtlgjs016@naver.com>
2024-12-05 01:01:28 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.42 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,33 +1,25 @@
 function solution(k, score) {
     let answer = [];
-    // 명예의 전당 목록
-    let minValue = [];
-    
-    // score 길이(7일) 만큼 반복
+    let temp = [];
     for(let i = 0; i < score.length; i++){
         
-        // 3일까지는 그냥 값 넣기
-        if(minValue.length < k){
-            minValue.push(score[i]);
-            answer.push(Math.min(...minValue));
-            
-        // 명예의 전당 목록 점수 중 가장 낮은 점수와 score[i] 비교
-        // score[i]가 더 크면 가장 낮은 값 빼고 score[i] 값 추가
-        } else if (Math.min(...minValue) < score[i]){
-            // 명예의 전당 배열 오름차순 정렬
-            minValue.sort((a, b) => a - b);
-            // 명예의 전당 배열에 제일 앞 요소 빼기
-            minValue.shift();
-            // 명예의 전당 배열에 score[i] 값 넣기
-            minValue.push(score[i]);
-            // answer 배열에 명예의 전당 배열값 중 가장 낮은 요소 값 넣기
-            answer.push(Math.min(...minValue));
-        // 위 경우가 다 false 인 경우()
+        // 임시 배열의 요소 개수가 k보다 작을 경우
+        if(temp.length < k){
+            // 임시 배열에 현재 값 추가
+            temp.push(score[i]);
         } else {
-            // answer 배열에 명예의 전당 배열 값 중 가장 낮은 요소 값 넣기
-            answer.push(Math.min(...minValue));
+            // 요소 중 가장 작은 값보다 현재 요소가 더 클 경우
+            if(Math.min(...temp) < score[i]){
+                // temp의 맨 앞 요소 제거
+                temp.shift();
+                // temp에 현재 요소 추가
+                temp.push(score[i]);
+            }
         }
+        // 결과 배열에 temp의 요소 중 가장 작은 값 추가
+        answer.push(Math.min(...temp));
+        // shift와 pop를 이용하기 위한 정렬
+        temp.sort((a, b) => a - b);
     }
     return answer;
-    
 }
