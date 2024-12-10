17531bf8a73de3ea0977b7a7774daa7b7bfcffc2
NohSiHeon <shtlgjs016@naver.com>
2024-12-10 20:46:29 +0900

    [level 1] Title: 2016년, Time: 5.35 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,18 +1,11 @@
 function solution(a, b) {
     let answer = '';
-    // 요일 형식 지정
-    const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    const day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
     
-    // 2016, a, b
-    // ex) 2016, 5, 24
-    let dateStr = '2016, ' + a + ', '+ b;
+    const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     
-    // Date 객체 만들기
-    let date = new Date(dateStr);
-    
-    // getDay를 이용해서 0 ~ 6 사이 숫자 값 구하기
-    // 만든 요일 배열의 인덱스에 date.getDay() 한것을 대입
-    answer = day[date.getDay()];
-    
+    const dDay = new Date(month[a - 1] + " " + b + " " + 2016);
+
+    answer = day[dDay.getDay()];
     return answer;
 }
